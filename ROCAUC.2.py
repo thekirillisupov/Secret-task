62ca3b63aa998143ba872b01301053d69243b108
Kirill <wonderful.kira@gmail.com>
2022-08-16 13:37:32 +0300

    modified:   .gitignore 	modified:   ROCAUC.2.py

--- ROCAUC.2.py
+++ ROCAUC.2.py
@@ -36,7 +36,8 @@ def write(*args, **kwargs):
 
 class MergeSort:
     @classmethod
-    def mergeSort(cls, arr, n):
+    def mergeSort(cls, _arr, n):
+        arr = list(_arr)
         temp_arr = [0] * n
         return cls._mergeSort(arr, temp_arr, 0, n - 1)
 
@@ -52,9 +53,9 @@ class MergeSort:
 
     @classmethod
     def merge(cls, arr, temp_arr, left, mid, right):
-        i = left    
-        j = mid + 1  
-        k = left     
+        i = left
+        j = mid + 1
+        k = left
         inv_count = 0
 
         while i <= mid and j <= right:
@@ -64,7 +65,7 @@ class MergeSort:
                 i += 1
             else:
                 temp_arr[k] = arr[j]
-                inv_count += (mid-i + 1)
+                inv_count += mid - i + 1
                 k += 1
                 j += 1
 
@@ -77,11 +78,12 @@ class MergeSort:
             temp_arr[k] = arr[j]
             k += 1
             j += 1
-    
+
         for loop_var in range(left, right + 1):
             arr[loop_var] = temp_arr[loop_var]
-    
+
         return inv_count
+
 
 class Solution:
     def __init__(self):
@@ -102,12 +104,12 @@ class Solution:
         sorted_by_value_target = cls._sort_target_in_each_equal_range_value(
             sorted_value, sorted_by_value_target
         )
-        
+
         deb0 = cls._number_of_non_inversions(sorted_by_value_target)
         deb2 = cls._number_of_non_inversions(sorted_target)
         deb3 = cls._count_combinations_of_pairs(sorted_target)
         deb4 = cls._count_equal_target(sorted_value, sorted_by_value_target)
-        
+
         numerator = deb0 - deb3 - deb4 / 2
         denominator = deb2 - deb3
 
@@ -115,11 +117,7 @@ class Solution:
 
     @classmethod
     def roc_auc_less_memory(cls, target: List[float], value: List[float]) -> float:
-        """
-        Calculate ROCAUC
 
-        find number inversion in index_sorted_by_value_target
-        """
         index_sorted_value = np.argsort(value)
         sorted_value = np.array(value)[index_sorted_value]
         sorted_by_value_target = np.array(target)[index_sorted_value]
@@ -127,21 +125,11 @@ class Solution:
             sorted_value, sorted_by_value_target
         )
 
-        mergeSort = MergeSort()
-
-        ts = time()
         deb4 = cls._count_equal_target(sorted_value, sorted_by_value_target)
-        print('deb4', time()-ts)
-        ts = time()
-        deb0 = mergeSort.mergeSort(sorted_by_value_target, len(sorted_by_value_target))
-        print('deb0', time()-ts)
-        ts = time()
-        deb2 = int(cls._number_of_pairs(sorted_by_value_target))
-        print('deb2', time()-ts)
-        ts = time()
+        deb0 = MergeSort.mergeSort(sorted_by_value_target, len(sorted_by_value_target))
+        sorted_by_value_target.sort()
+        deb2 = int(cls._number_of_all_pairs(sorted_by_value_target))
         deb3 = cls._count_combinations_of_pairs(sorted_by_value_target)
-        print('deb3', time()-ts)
-        ts = time()
 
         numerator = deb2 - deb0 - deb3 - deb4 / 2
         denominator = deb2 - deb3
@@ -149,11 +137,14 @@ class Solution:
         return numerator / denominator
 
     @classmethod
-    def _number_of_pairs(cls, arr: List[float]) -> int:
-        return len(arr)*(len(arr) - 1)/2
+    def _number_of_all_pairs(cls, arr: List[float]) -> int:
+        return len(arr) * (len(arr) - 1) / 2
 
     @classmethod
     def _sort_target_in_each_equal_range_value(cls, sorted_value, sorted_target):
+        '''
+        Complexity: k* n/k*log(n/k) = n*log(n/k)
+        '''
         right = 0
         left = 0
         res = [-1 for _ in range(len(sorted_target))]
@@ -168,6 +159,11 @@ class Solution:
 
     @classmethod
     def _count_combinations_of_pairs(cls, sorted_arr: List[float]) -> int:
+        """
+        return the number of pairs of identical elements
+
+        Complexity: O(n)
+        """
         res = 0
         right = 0
         left = 0
@@ -183,40 +179,22 @@ class Solution:
             left = right
 
     @classmethod
-    def _number_of_non_inversions(cls, arr: List[float]) -> int:
-        N = len(arr)
-        if N <= 1:
-            return 0
-
-        sortList = []
-        result = 0
-
-        for i, v in enumerate(arr):
-            heappush(sortList, (v, i))
-
-        x = []
-        while sortList:
-            v, i = heappop(sortList)
-            y = bisect(x, i)
-            result += y
-
-            insort(x, i)
-
-        return result
-
-    @classmethod
     def _number_of_non_inversions_in_equal_target(cls, arr: List[float]) -> int:
+        '''
+        Complexity: n*log(n)
+        ''' 
         try:
-            return cls._number_of_non_inversions(
-                arr
-            ) - cls._count_combinations_of_pairs(arr)
+            return cls._number_of_all_pairs(arr) - MergeSort.mergeSort(arr, len(arr)) -\
+                        cls._count_combinations_of_pairs(arr)
         except:
             return 0
 
+
     @classmethod
-    def _count_equal_target(
-        cls, _sorted_value: List[float], _sorted_by_value_target: List[float]
-    ) -> int:
+    def _count_equal_target(cls, _sorted_value: List[float], _sorted_by_value_target: List[float]) -> int:
+        '''
+        Complexity: k* n/k*log(n/k) = n*log(n/k)
+        '''
         res = 0
         right = 0
         left = 0
@@ -233,28 +211,6 @@ class Solution:
             )
             left = right
 
-
-sol = Solution()
-"""
-print(sol.roc_auc(target=[0, 1], value=[0, 1])) 
-print(sol.roc_auc(target=[0, 1], value=[1, 0]))
-print(sol.roc_auc(target=[0.5, 0.5, 2], value=[0, 1, 0.5]))
-print(sol.roc_auc(target=[0, 0, 1, 0, 1, 1, 1], value=[0, 1, 2, 3, 4, 5, 6]))
-print(sol.roc_auc(target=[0, 3, 1, 2, 1, 2, 4, 2, 4, 0], value=[4, 0, 2, 4, 0, 1, 1, 1, 4, 0]))
-print(sol.roc_auc(target=[3, 3, 2, 3, 4, 0, 2, 1, 4, 3], value=[4, 4, 1, 2, 1, 2, 0, 2, 4, 3]))
-"""
-
-'''
-arr1 = [4, 0, 2, 4, 0, 1, 1, 1, 4, 0]
-arr2 = [4, 0, 2, 4, 0, 1, 1, 1, 4, 0]
-mergeSort = MergeSort
-res = mergeSort.mergeSort(arr1, len(arr1))
-all_comb = (len(arr1))*(len(arr1) - 1)/2
-print(all_comb - res, arr1)
-print(sol._number_of_non_inversions(arr2))
-'''
-
-
 DEBUG = True
 
 if not DEBUG:
@@ -265,9 +221,9 @@ if not DEBUG:
         t, v = line.split(" ")
         target.append(t)
         value.append(v)
-    write(sol.roc_auc(target=target, value=value))
+    write(Solution.roc_auc(target=target, value=value))
 else:
-    #tracemalloc.start()
+    # tracemalloc.start()
     with open("input.txt", "r") as input_file:
         ts = time()
         n = input_file.readline()
@@ -278,9 +234,9 @@ else:
             target.append(t)
             value.append(v)
         with open("output.txt", "w") as output_file:
-            #sol.roc_auc_less_memory(target=target, value=value)
-            output_file.write(str(sol.roc_auc_less_memory(target=target, value=value)))
+            # sol.roc_auc_less_memory(target=target, value=value)
+            output_file.write(str(Solution.roc_auc_less_memory(target=target, value=value)))
         # write(sol.roc_auc(target=target, value=value))
-        write('finnaly',time() - ts)
-        #write(tracemalloc.get_traced_memory())
-        #tracemalloc.stop()
+        #write("finnaly", time() - ts)
+        # write(tracemalloc.get_traced_memory())
+        # tracemalloc.stop()
